2ae10ddf7bd4a942017b95c6ff943526f9dfa65c
Franck Magnan <fmagnan@gmail.com>
2015-01-22 15:57:39 +0100

    use best practices on controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -21,5 +22,25 @@ class DefaultController extends Controller
     public function adminAction()
     {
         return $this->render('default/admin.html.twig');
+    }
+
+    /**
+     * @Route("/users/", name="users")
+     */
+    public function displayUsersAction()
+    {
+        $users = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findAll();
+
+        return $this->render('default/users.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/user/{id}", name="user_profile")
+     */
+    public function showUserAction(User $user)
+    {
+        return $this->render('default/user_profile.html.twig', ['user' => $user]);
     }
 }
